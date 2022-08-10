ee0d366a1331b2164c2bc1172994ac520e7133d9
jychan99 <108382087+jychan99@users.noreply.github.com>
2022-08-10 16:16:25 +0900

    clock
    clock

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,9 @@
 const clock = document.querySelector("h2.clock");
 
-function sayhello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setTimeout(sayhello, 3000);
+getClock();
+setInterval(getClock, 1000);

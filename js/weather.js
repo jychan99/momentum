bafe2eb70ebed51231282c7cc29ed80701c8904c
jychan99 <108382087+jychan99@users.noreply.github.com>
2022-08-14 23:32:56 +0900

    weatherAPI
    fetch... nexttime maybe

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,17 @@
+const API_KEY = "ada18f161eb1e575b6981ffe9e89750c";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((Response) => Response.json())
+    .then((data) => {
+      const weather = document.querySelector(".weather span:first-child");
+      const city = document.querySelector(".weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoErr() {

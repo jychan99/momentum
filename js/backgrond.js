d5876c37039f80c0a78f49e656ddae65e49f4d18
jychan99 <108382087+jychan99@users.noreply.github.com>
2022-08-18 13:58:05 +0900

    my startingpage
    login & greeting fontsetting

--- js/backgrond.js
+++ js/backgrond.js
@@ -1,4 +1,23 @@
-const image = ["0.jpg", "1.jpg", "2.jpg"];
+const image = [
+  "0.jpeg",
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg",
+  "7.jpg",
+  "8.jpg",
+  "9.jpg",
+  "10.jpg",
+  "11.jpg",
+  "12.jpg",
+  "13.jpg",
+  "14.jpg",
+  "15.jpg",
+  "16.jpg",
+  "17.jpg",
+];
 
 const chosenImage = image[Math.floor(Math.random() * image.length)];
 
@@ -7,3 +26,5 @@ const backgroundImage = document.createElement("img");
 backgroundImage.src = `image/${chosenImage}`;
 
 document.body.appendChild(backgroundImage);
+
+backgroundImage.classList.add("wallpaper");
--- js/quotes.js
+++ js/quotes.js
@@ -1,35 +1,35 @@
 const quotes = [
   {
     quote: "Always do what you are afraid to do.",
-    author: "Ralph Waldo Emerson",
+    author: "「Ralph Waldo Emerson」",
   },
   {
     quote: "I'm going to live every minute of it.",
-    author: "Pixar-Soul",
+    author: "「Pixar-Soul」",
   },
   {
     quote: "To infinity and beyond!",
-    author: "Pixar-ToyStory",
+    author: "「Pixar-ToyStory」",
   },
   {
     quote: "Every thing is relative.",
-    author: "Albert Einstein",
+    author: "「Albert Einstein」",
   },
   {
     quote: "Imagination is more important than knowledge.",
-    author: "Albert Einstein",
+    author: "「Albert Einstein」",
   },
   {
     quote: "Sometimes the smallest things take up the most room in your heart.",
-    author: "Winnie the Pooh",
+    author: "「Winnie the Pooh」",
   },
   {
     quote: "The noblest art is that of making others happy.",
-    author: "P.T. Barnum",
+    author: "「P.T. Barnum」",
   },
   {
     quote: "Life is a series of building, testing, changing and iterating.",
-    author: "Lauren Mosenthal",
+    author: "「Lauren Mosenthal」",
   },
 ];
 
